af127877feb3243aae46f5fdcae273300592c4a1
Timo Stricker <TimoStricker@Timos-MacBook-Pro.local>
2015-09-22 13:44:59 -0500

    Fragments are working now
    fiiiinaly!

--- Baikio/app/src/main/java/de/baikio/MainActivity.java
+++ Baikio/app/src/main/java/de/baikio/MainActivity.java
@@ -1,18 +1,28 @@
 package de.baikio;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements MapFragment.OnFragmentInteractionListener, CommunityFragment.OnFragmentInteractionListener {
     private DrawerLayout mDrawer;
     private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private View mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +35,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Find our drawer view
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mView = (View) findViewById(R.id.flContent);
 
         // Set the menu icon instead of the launcher icon.
         final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.mipmap.ic_menu);
         ab.setDisplayHomeAsUpEnabled(true);
+
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
 
     }
 
@@ -42,12 +58,66 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
 
+
+
         return super.onOptionsItemSelected(item);
     }
+
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the planet to show based on
+        // position
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = MapFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = CommunityFragment.class;
+                break;
+            default:
+                fragmentClass = MapFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // Do different stuff
     }
 
 
